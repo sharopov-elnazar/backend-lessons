591b0dd41269c8dc7cde01ac40be1a3485d70d31
iqbolshoh <iilhomjonov777@gmail.com>
2025-04-29 08:59:00 +0500

    class

--- 4-class/index.php
+++ 4-class/index.php
@@ -10,8 +10,24 @@ class Human
         $this->name = $name;
         $this->age = $age;
     }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function __destruct()
+    {
+        echo "Object {$this->name} is destroyed\n";
+    }
 }
 
-$human = new Human('John Doe', 30);
-echo $human->name;
-$human->name = "Elnazar";
+$human = new Human('Elnazar', 30);
+$human2 = new Human('Azizbek', 25);
+
+print_r($human);
+
+echo $human->name . " " . $human->getAge();
+
+// 5 ta class yaratish
+// 3 ta xudusiyat 4 ta metod
